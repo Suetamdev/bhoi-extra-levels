2eb4313a620b6337ec9118496ea7b5b00e1b4806
Suetamdev <mateus-fuzetto@hotmail.com>
2025-07-21 02:25:46 -0300

    feat(first-commit): :sparkles: creating new mod BHOI - Extra Levels

---
+++ common/defines/bhoi_extra_levels.lua
@@ -0,0 +1,29 @@
+-- Military
+NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false
+
+-- Project
+NDefines.NProject.SCIENTIST_SKILL_LEVEL_THRESHOLDS = {
+    50,		
+    100,		
+    200,
+    300,
+    400,
+    500,
+    600, 
+    700,
+    900,
+    1000
+}
+NDefines.NProject.SCIENTIST_SKILL_LEVEL_SPEED_MODIFIER = {
+    -1.0,
+	-0.05,
+	0.05,
+	0.1,
+	0.15,
+	0.2,
+	0.25,
+	0.3,
+	0.35,
+    0.4,
+    0.5
+}
---
+++ descriptor.mod
@@ -0,0 +1,7 @@
+version="1.0"
+tags={
+	"Balance"
+}
+name="BHOI - Extra Levels"
+supported_version="1.16.*"
+remote_file_id="3530888506"
